9399e78490d795b551aa16bbfc1230fbfbc96b73
Kane <dmccer@gmail.com>
2015-10-29 15:44:46 +0800

    使用代理抓取车型文件

--- models.js
+++ models.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var request = require('request');
 var common = require('./common');
+var Pooler = require('./pooler');
 
 // 根据车系获取车款
 // 车款获取地址: http://meta.che300.com/meta/model/model_series213.json?v=55
@@ -9,42 +10,13 @@ function build_model_url(series_id) {
   return 'http://meta.che300.com/meta/model/model_series' + series_id + '.json?v=57'
 }
 
-function req(url, callback) {
-  var options = {
-    url: url,
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36'
-    }
-  };
-
-  request(options, function(err, res, body) {
-    if (err) {
-      console.log('请求失败');
-      console.log(err);
-
-      callback.call(this, null, err);
-
-      return;
-    }
-
-    if (res.statusCode !== 200) {
-      console.log('请求失败');
-      console.log('响应状态码:', res.statusCode);
-
-      callback.call(this, null, res);
-      return;
-    }
-
-    callback.call(this, body);
-  });
-}
-
 exports.crawl = function() {
-  console.log('-- 开始抓取车型 --');
+  console.log('-- 开始根据车系抓取车型 --');
 
   var files = fs.readdirSync(common.series_path);
   var count = 0;
   var complete = 0;
+  var total = 0;
 
   files.forEach(function(file) {
     var series = JSON.parse(fs.readFileSync(path.join(common.series_path, file)).toString());
@@ -54,29 +26,25 @@ exports.crawl = function() {
     series.forEach(function(_series) {
       var url = build_model_url(_series.series_id);
 
-      req(url, function(body) {
-        complete++;
-
-        if (body == null) {
-          return;
+      Pooler.add({
+        url: url,
+        callback: function(body) {
+          complete++;
+          fs.writeFileSync(path.join(common.model_path, _series.series_id) + '.json', body);
+          total += JSON.parse(body).length;
         }
-
-        fs.writeFileSync(path.join(common.model_path, _series.series_id) + '.json', body);
       });
     });
   });
 
-  var timer = setInterval(function() {
-    if (count === complete) {
-      clearInterval(timer);
+  Pooler.done = function() {
+    console.log('车型抓取完成');
+    console.log('车系量: ' + count);
+    console.log('完成量: ' + complete);
+    console.log('车型量: ' + total);
+  }
 
-      console.log('车型抓取完成');
-      console.log('车系量: ' + count);
-      console.log('完成量: ' + complete);
-
-      return;
-    }
-  }, 500);
+  Pooler.run();
 }
 
 this.crawl();
