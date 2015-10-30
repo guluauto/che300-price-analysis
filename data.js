56e5eb775f096095b7caf4df05776073899006e6
Kane <dmccer@gmail.com>
2015-10-30 13:25:53 +0800

    固定数据文件

---
+++ data.js
@@ -0,0 +1,52 @@
+var SERIES_PATH = './series';
+var MODEL_PATH = './models'
+
+var CITYS = [
+  {
+    name: '西安',
+    id: 'v28c27',
+    cid: '27',
+    pid: '28'
+  }, {
+    name: '上海',
+    id: 'v3c3',
+    cid: '3',
+    pid: '3'
+  }, {
+    name: '北京',
+    id: 'v1c1',
+    cid: '1',
+    pid: '1'
+  }, {
+    name: '重庆',
+    id: 'v4c4',
+    cid: '4',
+    pid: '4'
+  }, {
+    name: '成都',
+    id: 'v22c22',
+    cid: '22',
+    pid: '22'
+  }, {
+    name: '广州',
+    id: 'v20c20',
+    cid: '20',
+    pid: '20'
+  }, {
+    name: '深圳',
+    id: 'v20c50',
+    cid: '50',
+    pid: '20'
+  }, {
+    name: '杭州',
+    id: 'v12c12',
+    cid: '12',
+    pid: '12'
+  }
+];
+
+module.exports = {
+  series_path: SERIES_PATH,
+  model_path: MODEL_PATH,
+  citys: CITYS
+}
